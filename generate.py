bd26f433bc46e4a63dac71d423135d47faf63168
reidsteven75 <reidsteven75@gmail.com>
2019-02-14 21:38:46 -0700

    update

--- generate.py
+++ generate.py
@@ -44,7 +44,7 @@ def compute_embeddings():
     for fname in os.listdir('audio_input'):
       if 'aif' in fname:
         nfn = 'audio_input/'+fname.replace('aif', 'wav')
-        subprocess.call(["sox", source_dir+'/audio_input/'+fname, "-b", "16", "-r", "16000", "-c", "1", nfn])
+        subprocess.call(["sox", 'audio_input/'+fname, "-b", "16", "-r", "16000", "-c", "1", nfn],shell=True)
         os.rename('audio_input/'+fname, 'aif_bkp/'+fname)
 
   subprocess.check_call(["nsynth_save_embeddings",

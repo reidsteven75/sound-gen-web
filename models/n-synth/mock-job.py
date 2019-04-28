7990d10cc3b84b98558bf00ccc3462fee9eccea2
reidsteven75 <reidsteven75@gmail.com>
2019-04-27 22:36:10 -0600

    update

--- models/n-synth/mock-job.py
+++ models/n-synth/mock-job.py
@@ -12,12 +12,15 @@ print("--------")
 # Storage = AI model checkpoint(s)
 src = os.getcwd() + '/storage'
 dst = os.getcwd() + '/job/storage'
+if (not os.path.isdir(src)):
+  os.mkdir(src)
 os.symlink(src, dst)
 
 # Artifact = job output
 src = os.getcwd() + '/artifacts'
 dst = os.getcwd() + '/job/artifacts'
+if (not os.path.isdir(src)):
+  os.mkdir(src)
 os.symlink(src, dst)
-
 # Start job
 subprocess.call(["python", "generate.py"], cwd="job")

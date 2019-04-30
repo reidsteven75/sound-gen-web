3d00bf64a4d2259a201dd7916a8c6f38b7bbe2ba
reidsteven75 <reidsteven75@gmail.com>
2019-04-30 12:51:33 -0600

    structure cleanup

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ g-synth cpu
 docker-compose -f docker-compose-g-synth.yml up --build
 ```
 
+kubernetes
+```
+minikube start --bootstrapper=localkube
+```
+
 # Deployment 
 Deployed as a job that runs in Paperspace. Job runs in a configurable docker container and uses configurable AI model checkpoint(s) that has been upload as .zip file to Paperspace 'storage' (common persistant storage)
 
@@ -24,6 +29,7 @@ $ ./deploy-job.sh
 # Paperspace
 CLI/API
 https://paperspace.github.io/paperspace-node/
+https://docs.paperspace.com/gradient/experiments/run-experiments
 
 GPU & CPU Types
 https://support.paperspace.com/hc/en-us/articles/360007742114-Gradient-Instance-Types
--- models/n-synth/job/generate.py
+++ models/n-synth/job/generate.py
@@ -57,15 +57,15 @@ def init():
 def compute_embeddings():
 	subprocess.call(["nsynth_save_embeddings", 
 		"--checkpoint_path=%s/model.ckpt-200000" %(checkpoint_dir), 
-		"--source_path=input", 
-		"--save_path=embeddings_input", 
+		"--source_path=data/input", 
+		"--save_path=data/embeddings_input", 
 		"--batch_size=32"])
 
 def compute_new_embeddings():
 	#	constants and rearrangement of settings vars for processing
 	pitches = settings['pitches']
 	resolution = settings['resolution']
-	final_length = settings['final_length']
+	final_length = settings['sample_length']
 	instrument_groups = [settings['pads']['NW'], settings['pads']['NE'], settings['pads']['SE'], settings['pads']['SW']]
 	combinations = sorted(product(*instrument_groups))
 	xy_grid = make_grid(resolution)
@@ -73,7 +73,7 @@ def compute_new_embeddings():
 	#	cache all embeddings
 	embeddings_lookup = {}
 
-	for filename in os.listdir('embeddings_input/'):
+	for filename in os.listdir('data/embeddings_input/'):
 		# ignore all non-npy files
 		if '.npy' in  filename:
 			#	convert filename to reference key
@@ -81,7 +81,7 @@ def compute_new_embeddings():
 			reference = '{}_{}'.format(parts[0], parts[1])
 
 			#	load the saved embedding
-			embeddings_lookup[reference] = np.load("embeddings_input/%s" % filename)
+			embeddings_lookup[reference] = np.load("data/embeddings_input/%s" % filename)
 
 	def get_embedding(instrument, pitch):
 		reference = '{}_{}'.format(instrument, pitch)
@@ -110,36 +110,36 @@ def compute_new_embeddings():
 				#	reshape array
 				# interpolated = np.reshape(interpolated, (1,) + interpolated.shape)
 
-				np.save('embeddings_output/' + name + '.npy', interpolated.astype(np.float32))
+				np.save('data/embeddings_output/' + name + '.npy', interpolated.astype(np.float32))
 
 def batch_embeddings():
-	num_embeddings = len(os.listdir('embeddings_output'))
+	num_embeddings = len(os.listdir('data/embeddings_output'))
 	batch_size = num_embeddings / settings['gpus']
 	#	split the embeddings per gpu in folders
 	for i in range(0, settings['gpus']):
-		foldername = 'embeddings_batched/batch%i' % i
+		foldername = 'data/embeddings_batched/batch%i' % i
 		if not os.path.exists(foldername):
 			os.mkdir(foldername)
-		output_foldername = 'audio_output/batch%i' % i
+		output_foldername = 'data/audio_output/batch%i' % i
 		if not os.path.exists(output_foldername):
 			os.mkdir(output_foldername)
 
 	#	shuffle to the folders
 	batch = 0
-	for filename in os.listdir('embeddings_output'):
-		target_folder = 'embeddings_batched/batch%i/' % batch
+	for filename in os.listdir('data/embeddings_output'):
+		target_folder = 'data/embeddings_batched/batch%i/' % batch
 		batch += 1
 		if batch >= settings['gpus']:
 			batch = 0
 
-		os.rename('embeddings_output/' + filename, target_folder + filename)
+		os.rename('data/embeddings_output/' + filename, target_folder + filename)
 
 def gen_call(gpu):
 	return subprocess.call(["nsynth_generate",
 		"--checkpoint_path=%s/model.ckpt-200000" %(checkpoint_dir),
-		"--source_path=embeddings_batched/batch%i" % gpu,
-		"--save_path=audio_output/batch%i" % gpu,
-		"--sample_length=32000",
+		"--source_path=data/embeddings_batched/batch%i" % gpu,
+		"--save_path=data/audio_output/batch%i" % gpu,
+		"--sample_length=%s" %(settings['sample_length']),
 		"--encodings=true",
 		"--log=INFO",
 		"--batch_size=512"])
@@ -153,8 +153,8 @@ def generate_audio():
 	pbar = tqdm(len(os.listdir('embeddings_output')))
 	pbar.set_description("Number of files for which processing has started")
 	while not results.ready():
-		num_files = sum([len(os.listdir('audio_output/batch%s' %(i))) for i in range(settings['gpus'])])
-		num_files = len(os.listdir('audio_output'))
+		num_files = sum([len(os.listdir('data/audio_output/batch%s' %(i))) for i in range(settings['gpus'])])
+		num_files = len(os.listdir('data/audio_output'))
 		pbar.update(num_files - pbar.n)
 		time.sleep(1)
 	pbar.close()
@@ -162,7 +162,7 @@ def generate_audio():
 	pool.join()
 
 	for i in range(0, settings['gpus']):
-		source = 'audio_output/batch%i/' % i
+		source = 'data/audio_output/batch%i/' % i
 		files = os.listdir(source)
 		for f in files:
 			shutil.move(source + f, output_dir)

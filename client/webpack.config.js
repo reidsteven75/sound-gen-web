e8cd70e353a32f9cd26f75fc458c8d245ddee022
reidsteven75 <reidsteven75@gmail.com>
2019-05-15 20:46:27 -0600

    react create app removal, added webpacl

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,38 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/public',
     publicPath: '/',
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      PUBLIC_URL: './',
+      filename: 'public/index.html'
+    })
+  ],
   devServer: {
-    contentBase: './dist'
+    contentBase: './public',
+    hot: true
   }
 }

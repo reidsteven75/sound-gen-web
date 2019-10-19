28fbe3788006b1273090c8a81af266c4e6bec215
reidsteven75 <reidsteven75@gmail.com>
2019-10-19 16:35:42 -0600

    remove data processing & cli

--- models/n-synth/deploy-job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-paperspace jobs create \
-    --machineType 'GPU+' \
-    --container 'reidsteven75/sound-gen-n-synth:latest'\
-    --command 'python generate.py'\
-    --workspace './job'
